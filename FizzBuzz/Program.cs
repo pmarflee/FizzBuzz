01f103a405b9327603bcf9e3d7eecba96164583c
pmarflee <paul.marfleet@gmail.com>
2015-07-16 11:19:29 +0100

    Created repo

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Write a program that prints the numbers from 1 to 100
+             * But for multiples of three print "Fizz" instead of the number 
+             * and for the multiples of five print "Buzz".
+             * For numbers which are multiples of both three and five print "FizzBuzz"
+             */
+        }
+    }
+}
